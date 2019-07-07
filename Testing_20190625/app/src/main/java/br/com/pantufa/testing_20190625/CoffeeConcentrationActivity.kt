cf3d3c9e8a16300acb9cb9c94dd74a1fc2295de5
Unknown <yuri.moreeuw@gmail.com>
2019-07-07 17:46:35 -0300

    Added screens of concentration and extraction

---
+++ Testing_20190625/app/src/main/java/br/com/pantufa/testing_20190625/CoffeeConcentrationActivity.kt
@@ -0,0 +1,12 @@
+package br.com.pantufa.testing_20190625
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeConcentrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee_concentration)
+    }
+}
---
+++ Testing_20190625/app/src/main/java/br/com/pantufa/testing_20190625/CoffeeExtractionActivity.kt
@@ -0,0 +1,12 @@
+package br.com.pantufa.testing_20190625
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeExtractionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee_extraction)
+    }
+}
