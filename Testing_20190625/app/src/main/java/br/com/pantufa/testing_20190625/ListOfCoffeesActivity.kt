17f855f6f98c689e7f7702fd4ffa8b0a876b7033
Yuri de Azevedo Machioni <yurimachioni@gmail.com>
2019-07-13 23:01:53 -0300

    Simple recyclerView integration example in the coffee list

--- Testing_20190625/app/src/main/java/br/com/pantufa/testing_20190625/ListOfCoffeesActivity.kt
+++ Testing_20190625/app/src/main/java/br/com/pantufa/testing_20190625/ListOfCoffeesActivity.kt
@@ -3,6 +3,7 @@ package br.com.pantufa.testing_20190625
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_list_of_coffees.*
 
 class ListOfCoffeesActivity : AppCompatActivity() {
@@ -13,10 +14,15 @@ class ListOfCoffeesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_of_coffees)
 
-        coffeeBeanRepository.getCoffeeList ({
-            Toast.makeText(this, it.first().producer, Toast.LENGTH_SHORT).show()
 
-            text_variety.text = it.first().variety
+
+        coffeeBeanRepository.getCoffeeList ({ list ->
+            //o recyclerView nao sabe sozinho como mostrar os dados de uma lista qualquer num layout qualquer.
+            //aqui mandamos ele usar o nosso adapter que diz exatamente como criar nossos items e como vincular os dados às views.
+            listOfCoffesRecycler.adapter = ListOfCoffeesAdapter(list)
+
+            //esse cara diz pro recyclerView exibir nossos dados linearmente. tambem poderiamos passar GridLayoutManager aqui.
+            listOfCoffesRecycler.layoutManager = LinearLayoutManager(this)
         }, {
             Toast.makeText(this, "Puts, erro no servico", Toast.LENGTH_SHORT).show()
         })
